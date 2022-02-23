470ba35741ca3760bfacfe1ddd40a14f3db76cd5
qthequartermasterman <qthequartermasterman@gmail.com>
2022-02-23 15:06:31 -0600

    Implemented refresh_time in DataCollectionUtils.py

--- ElectionForecasting/data_collection/DataCollectionUtils.py
+++ ElectionForecasting/data_collection/DataCollectionUtils.py
@@ -1,5 +1,7 @@
 from functools import wraps
 import pandas as pd
+import os
+import time
 
 
 def cache_download_csv_to_file(filename, refresh_time=None):
@@ -10,11 +12,16 @@ def cache_download_csv_to_file(filename, refresh_time=None):
         file anew. If refresh_time=None, the file will never be regenerated if a cached version exists.
     :return:
     """
+
     def cache_wrapper(func):
         @wraps(func)
         def inside_wrapper(*args, **kwargs):
             # TODO: Refresh the file if it has been longer than refresh_time
+
             try:
+                if refresh_time is not None and os.path.getmtime(filename) + int(refresh_time * 60 * 60) < time.time():
+                    print('File is too old')
+                    raise FileNotFoundError(f'{filename} is too old and needs to be refreshed')
                 dataframe: pd.DataFrame = pd.read_csv(filename, index_col=0)
             except FileNotFoundError:
                 dataframe = func(*args, **kwargs)
