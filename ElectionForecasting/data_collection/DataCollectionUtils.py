4104fd9555b64ad6025f993c71ce1d2b8df495a1
qthequartermasterman <qthequartermasterman@gmail.com>
2022-02-23 14:53:48 -0600

    Documentation on DataCollectionUtils.py

--- ElectionForecasting/data_collection/DataCollectionUtils.py
+++ ElectionForecasting/data_collection/DataCollectionUtils.py
@@ -2,10 +2,18 @@ from functools import wraps
 import pandas as pd
 
 
-def cache_download_csv_to_file(filename):
+def cache_download_csv_to_file(filename, refresh_time=None):
+    """
+    Save a pandas DataFrame to a csv file for faster caching.
+    :param filename: filepath to save the cached csv
+    :param refresh_time: Time in hours. If the file has not been updated in longer than refresh_time, generate the
+        file anew. If refresh_time=None, the file will never be regenerated if a cached version exists.
+    :return:
+    """
     def cache_wrapper(func):
         @wraps(func)
         def inside_wrapper(*args, **kwargs):
+            # TODO: Refresh the file if it has been longer than refresh_time
             try:
                 dataframe: pd.DataFrame = pd.read_csv(filename, index_col=0)
             except FileNotFoundError:
