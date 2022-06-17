a757a2383897918b43495a60e0209c7655bf2987
qthequartermasterman <qthequartermasterman@gmail.com>
2022-06-16 23:49:10 -0500

    add setup.py

---
+++ ElectionForecasting/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Election Forecasting',
+      version='0.1.0',
+      description='Election Forecasting',
+      author='Andrew Sansom',
+      author_email='andrew@euleriancircuit.com',
+      url='https://www.euleriancircuit.com/',
+      # packages=[],
+     )
