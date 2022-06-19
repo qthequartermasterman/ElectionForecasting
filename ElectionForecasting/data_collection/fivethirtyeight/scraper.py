73ce39170ff3fbff23eb58aa722bac28b0b4c9a2
qthequartermasterman <qthequartermasterman@gmail.com>
2022-06-18 23:05:09 -0500

    fivethirtyeight scraper functionality

--- ElectionForecasting/data_collection/fivethirtyeight/scraper.py
+++ ElectionForecasting/data_collection/fivethirtyeight/scraper.py
@@ -1,17 +1,72 @@
 from typing import Optional
-
+from datetime import date
 import pandas as pd
 
 import urls
 
 from ElectionForecasting.data_collection.DataCollectionUtils import cache_download_csv_to_file
 
+REFRESH_RATE = 0  # 12  # Refresh this every 12 hr
 
-@cache_download_csv_to_file('data/fivethirtyeight/house.csv')
-def get_raw_house_data(url: Optional[str] = None):
-    house_url = url or urls.house_polls
-    house_csv = pd.read_csv(house_url, index_col='poll_id')
-    house_csv['District'] = house_csv['state'] + house_csv['seat_number'].astype(str)
-    return house_csv
 
-get_raw_house_data()
+def rename_party(party: str) -> str:
+    """
+    Take fivethirtyeight house party labels and replace them with our standard names
+    :param party:
+    :return:
+    """
+    if party == 'REP':
+        return 'Republican'
+    if party == 'DEM':
+        return 'Democratic'
+    if party == 'LIB':
+        return 'Libertarian'
+    if party == 'GRN':
+        return 'Green'
+    return 'Independent'
+
+
+def str_to_date(date_str: str) -> date:
+    m, d, y = date_str.split('/')
+    m, d, y = int(m), int(d), 2000 + int(y)
+    print(m, d, y)
+    return date(y, m, d)
+
+
+def get_raw_poll_data(url: Optional[str] = None) -> pd.DataFrame:
+    poll_url = url or urls.house_polls
+    poll_csv = pd.read_csv(poll_url, index_col='poll_id')
+    poll_csv['District'] = poll_csv['state'] + poll_csv['seat_number'].astype(str)
+    poll_csv['party'] = poll_csv['party'].apply(rename_party)
+    poll_csv['end_date'] = poll_csv['end_date'].apply(str_to_date)
+    poll_csv['election_date'] = poll_csv['election_date'].apply(str_to_date)
+    return poll_csv
+
+
+def compile_raw_polls_to_timeseries(raw_poll_df: pd.DataFrame, party: str, election_date: date) -> pd.DataFrame:
+    compiled_df: pd.DataFrame = pd.DataFrame()
+    # Take each row, and put the poll results in the correct date column and district row
+    for index, row in raw_poll_df.iterrows():
+        print(row['election_date'], row['party'])
+        if row['election_date'] == election_date and row['party'] == party:
+            print(index, row)
+            compiled_df.loc[row['District'], row['end_date']] = row['pct']
+    compiled_df = compiled_df.sort_index()
+    compiled_df = compiled_df.sort_index(axis=1, ascending=True)
+    return compiled_df
+
+
+@cache_download_csv_to_file('data/fivethirtyeight/house_raw.csv', refresh_time=REFRESH_RATE)
+def get_raw_house_data(url: Optional[str] = None) -> pd.DataFrame:
+    return get_raw_poll_data(url)
+
+
+@cache_download_csv_to_file('data/fivethirtyeight/house_raw_poll_timeseries.csv', refresh_time=REFRESH_RATE)
+def compile_raw_house_data_to_timeseries(raw_poll_df: pd.DataFrame, party: str, election_date: date) -> pd.DataFrame:
+    return compile_raw_polls_to_timeseries(raw_poll_df, party, election_date)
+
+
+raw = get_raw_house_data()
+
+election_date = str_to_date('11/8/22')
+compiled = compile_raw_house_data_to_timeseries(raw, 'Republican', election_date)
