49862882ce0681d67fcb18df538d04974562a27a
qthequartermasterman <qthequartermasterman@gmail.com>
2022-06-20 19:40:12 -0500

    fivethirtyeight scraper average results from the same day and district

--- ElectionForecasting/data_collection/fivethirtyeight/scraper.py
+++ ElectionForecasting/data_collection/fivethirtyeight/scraper.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Optional
 from datetime import date
 import pandas as pd
@@ -36,7 +37,7 @@ def str_to_date(date_str: str) -> date:
 def get_raw_poll_data(url: Optional[str] = None) -> pd.DataFrame:
     poll_url = url or urls.house_polls
     poll_csv = pd.read_csv(poll_url, index_col='poll_id')
-    poll_csv['District'] = poll_csv['state'] + poll_csv['seat_number'].astype(str)
+    poll_csv['District'] = poll_csv['state'] + '-' + poll_csv['seat_number'].astype(str).str.pad(2, fillchar='0')
     poll_csv['party'] = poll_csv['party'].apply(rename_party)
     poll_csv['end_date'] = poll_csv['end_date'].apply(str_to_date)
     poll_csv['election_date'] = poll_csv['election_date'].apply(str_to_date)
@@ -46,11 +47,21 @@ def get_raw_poll_data(url: Optional[str] = None) -> pd.DataFrame:
 def compile_raw_polls_to_timeseries(raw_poll_df: pd.DataFrame, party: str, election_date: date) -> pd.DataFrame:
     compiled_df: pd.DataFrame = pd.DataFrame()
     # Take each row, and put the poll results in the correct date column and district row
+
+    district_date_counts = defaultdict(lambda: 0)
     for index, row in raw_poll_df.iterrows():
         print(row['election_date'], row['party'])
         if row['election_date'] == election_date and row['party'] == party:
+            # TODO: Logic about what to do if multiple polls for a district occur on the same date
+            # TODO: For now, just take the average
             print(index, row)
-            compiled_df.loc[row['District'], row['end_date']] = row['pct']
+            if row['District'] in compiled_df.index and row['end_date'] in compiled_df.columns:
+                compiled_df.loc[row['District'], row['end_date']] += row['pct']
+            else:
+                compiled_df.loc[row['District'], row['end_date']] = row['pct']
+                district_date_counts[(row['District'], row['end_date'])] += 1
+    for (district, date), count in district_date_counts.items():
+        compiled_df.loc[district, date] /= count
     compiled_df = compiled_df.sort_index()
     compiled_df = compiled_df.sort_index(axis=1, ascending=True)
     return compiled_df
