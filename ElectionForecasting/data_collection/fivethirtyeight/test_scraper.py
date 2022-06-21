efc4c69b78c7fd2b289afb52bfcd32ddaf61b5ca
qthequartermasterman <qthequartermasterman@gmail.com>
2022-06-20 20:18:50 -0500

    fivethirtyeight scraper start date

---
+++ ElectionForecasting/data_collection/fivethirtyeight/test_scraper.py
@@ -0,0 +1,20 @@
+from unittest import TestCase
+
+
+class Test(TestCase):
+    def test_rename_party(self):
+        self.fail()
+
+    def test_str_to_date(self):
+        self.fail()
+
+    def test_get_raw_poll_data(self):
+        self.fail()
+
+    def test_get_raw_generic_ballot_poll_data(self):
+        self.fail()
+
+    def test_compile_raw_polls_to_timeseries(self):
+        # TODO: Test start_date=None
+        # TODO: assert only polls after start_date appear
+        self.fail()
