47d8b56c2e60bb3e303f1b1b472493eaf7a05e41
qthequartermasterman <qthequartermasterman@gmail.com>
2022-06-28 15:33:12 -0500

    tests for PollsCompiler live data

--- tests/data_collection/polling/test_PollsCompiler.py
+++ tests/data_collection/polling/test_PollsCompiler.py
@@ -6,14 +6,20 @@ import pytest
 from ElectionForecasting.data_collection.polling.PollsCompiler import PollsCompiler
 
 
-def test_compile_raw_house_data_to_timeseries():
+def test_obtain_house_poll_timeseries():
+    election_date = datetime.date(2022, 11, 8)
+    start_date = datetime.date(2022, 3, 13)
     compiler = PollsCompiler()
-    pytest.fail()
+    compiler.obtain_house_poll_timeseries(party='Republican', election_date=election_date,
+                                          starting_date=start_date)
 
 
 def test_compile_raw_generic_ballot_data_to_timeseries():
+    election_date = datetime.date(2022, 11, 8)
+    start_date = datetime.date(2022, 3, 13)
     compiler = PollsCompiler()
-    pytest.fail()
+    compiler.obtain_generic_house_poll_timeseries(party='Republican', election_date=election_date,
+                                                  starting_date=start_date)
 
 
 def test_compile_raw_polls_to_timeseries_with_start_date():
@@ -25,13 +31,14 @@ def test_compile_raw_polls_to_timeseries_with_start_date():
 
     for col in compiled_df.columns:
         if isinstance(col, datetime.date):
-            assert col < start_date
-    # TODO: assert only polls after start_date appear
+            assert col > start_date  # Every column should be after the start_date
 
 
 def test_compile_raw_polls_to_timeseries_without_start_date():
     compiler = PollsCompiler()
+    start_date = datetime.date(2022, 3, 13)
     compiled_df = compiler.compile_raw_polls_to_timeseries(raw_generic_df, party='Republican',
-                                                           election_date=datetime.date(2022, 11, 8), )
-    pytest.fail()
+                                                           election_date=datetime.date(2022, 11, 8),
+                                                           starting_date=None)
+    assert any(col < start_date for col in compiled_df.columns if isinstance(col, datetime.date))
     # TODO: Test start_date=None
